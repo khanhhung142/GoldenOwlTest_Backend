91ed7ab84c823084e4d4d1044cc3bd171f082c5c
hung <phantrankhanhhung@gmail.com>
2023-02-03 09:02:41 +0700

    handle cors

--- main.go
+++ main.go
@@ -5,12 +5,24 @@ import (
 
 	"github.com/gorilla/mux"
 	shoeapi "github.com/khanhhung142/GoldenOwlTest_Backend/apis/shoeAPI"
+	"github.com/rs/cors"
 )
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/api/v1/shoeapi/getall", shoeapi.GetAllShoes).Methods("GET")
-	err := http.ListenAndServe(":5000", router)
+	cors := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{
+			http.MethodPost,
+			http.MethodGet,
+		},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: false,
+	})
+	router.HandleFunc("/shoe/getall", shoeapi.GetAllShoes).Methods("GET")
+
+	handler := cors.Handler(router)
+	err := http.ListenAndServe(":5000", handler)
 	if err != nil {
 		panic(err)
 	}
